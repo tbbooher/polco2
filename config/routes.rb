9b66065a6d961c56fb9bb3dd8d355d7735138ba1
Tim Booher <ads@theboohers.org>
2012-05-14 21:49:27 -0400

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,16 @@ Polco2::Application.routes.draw do
   resources :subjects
   resources :bills
 
+  # what bills are active?
   get "represent/house_bills"
   get "represent/senate_bills"
+  # how are the legislators voting?
+  get "represent/legislators_districts"
+  get "represent/states"
+  # how are you being represented?
+  get "represent/senate_results"
+  get "represent/house_results"
+  # helper to add vote
   post "bills/add_vote", as: :add_vote
 
   root :to => "home#index"
