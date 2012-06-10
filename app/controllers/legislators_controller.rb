a24ade471e3ccfa65faf5317408aacb2c3639bb3
Tim Booher <ads@theboohers.org>
2012-06-09 22:18:28 -0400

    updated pagination error

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,6 +1,6 @@
 class LegislatorsController < InheritedResources::Base
   def show
     super
-    @latest_votes = @legislator.latest_votes.paginate(params[:page]).per(10)
+    @latest_votes = @legislator.latest_votes.page(params[:page])
   end
 end
